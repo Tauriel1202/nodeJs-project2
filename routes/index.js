75d2ce53aea51ad3cd58957189133078a4fdc460
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-10-04 20:47:11 -0400

    graphql attempts

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const routes = require("express").Router();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("../schema");
 
 //start up test
 // const displayName = require('../controllers/index');
@@ -10,11 +12,21 @@ routes.use("/", swagger);
 
 //chars route
 const chars = require("./chars");
-routes.use("/characters", chars);
+// routes.use("/characters", chars);
 
 //quotes routes
 const quotes = require("./quotes");
 routes.use("/quotes", quotes);
+
+//graphql
+routes.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: null,
+    schema,
+    graphiql: true,
+  })
+);
 
 //export
 module.exports = routes;
---
+++ schema.js
@@ -0,0 +1,41 @@
+const graphql = require("graphql");
+const chars = require("./controllers/chars");
+
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLSchema,
+} = graphql;
+
+const charType = new GraphQLObjectType({
+  name: "char",
+  fields: () => ({
+    firstname: { type: GraphQLString },
+    lastname: { type: GraphQLString },
+    homeland: { type: GraphQLString },
+    currentHome: { type: GraphQLString },
+    species: { type: GraphQLString },
+    hairColor: { type: GraphQLString },
+    eyeColor: { type: GraphQLString },
+    favColor: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  }),
+});
+
+const rootQuery = new GraphQLObjectType({
+  name: "rootQueryType",
+  fields: {
+    char: {
+      type: charType,
+      resolve() {
+        return routes.get("/", chars.getAll);
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: rootQuery,
+});
